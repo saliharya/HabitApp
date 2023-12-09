7097e54bbda115153ed36c0fa0fe3e37610d9141
saliharya <saliharyazinc@gmail.com>
2023-12-09 20:25:56 +0700

    add primary key notation

--- app/src/main/java/com/dicoding/habitapp/data/Habit.kt
+++ app/src/main/java/com/dicoding/habitapp/data/Habit.kt
@@ -2,15 +2,16 @@ package com.dicoding.habitapp.data
 
 import android.os.Parcelable
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 //TODO 1 : Define a local database table using the schema in app/schema/habits.json
 @Parcelize
 @Entity(tableName = "habits")
 data class Habit(
-    val id: Int = 0,
+    @PrimaryKey val id: Int = 0,
     val title: String,
     val minutesFocus: Long,
     val startTime: String,
     val priorityLevel: String
-): Parcelable
+) : Parcelable
