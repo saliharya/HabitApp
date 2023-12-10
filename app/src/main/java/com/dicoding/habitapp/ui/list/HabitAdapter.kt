6e4ded969fddf4a517d0dbe29d80b344a7e37eda
saliharya <saliharyazinc@gmail.com>
2023-12-10 14:06:46 +0700

    fix priority color

--- app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
+++ app/src/main/java/com/dicoding/habitapp/ui/list/HabitAdapter.kt
@@ -29,7 +29,7 @@ class HabitAdapter(
     inner class HabitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         private val tvTitle: TextView = itemView.findViewById(R.id.item_tv_title)
-        val ivPriority: ImageView = itemView.findViewById(R.id.item_priority_level)
+        private val ivPriority: ImageView = itemView.findViewById(R.id.item_priority_level)
         private val tvStartTime: TextView = itemView.findViewById(R.id.item_tv_start_time)
         private val tvMinutes: TextView = itemView.findViewById(R.id.item_tv_minutes)
 
@@ -39,6 +39,13 @@ class HabitAdapter(
             tvTitle.text = habit.title
             tvStartTime.text = habit.startTime
             tvMinutes.text = habit.minutesFocus.toString()
+
+            when (habit.priorityLevel) {
+                "High" -> ivPriority.setImageResource(R.drawable.ic_priority_high)
+                "Medium" -> ivPriority.setImageResource(R.drawable.ic_priority_medium)
+                "Low" -> ivPriority.setImageResource(R.drawable.ic_priority_low)
+            }
+
             itemView.setOnClickListener {
                 onClick(habit)
             }
