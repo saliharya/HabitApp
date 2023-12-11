0010dc78e0f3d4dd2c8530199a78e555871e02ac
saliharya <saliharyazinc@gmail.com>
2023-12-11 10:36:59 +0700

    fix android 12 notification

--- app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
+++ app/src/main/java/com/dicoding/habitapp/notification/NotificationWorker.kt
@@ -1,9 +1,12 @@
 package com.dicoding.habitapp.notification
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
@@ -12,9 +15,27 @@ import com.dicoding.habitapp.ui.detail.DetailHabitActivity
 import com.dicoding.habitapp.utils.HABIT_TITLE
 import com.dicoding.habitapp.utils.NOTIFICATION_CHANNEL_ID
 
+@RequiresApi(Build.VERSION_CODES.O)
 class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
 
     private val habitTitle = inputData.getString(HABIT_TITLE)
+
+    init {
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID, "notify-channel", NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            description = NOTIFICATION_CHANNEL_ID
+        }
+
+        val notificationManager =
+            applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+    }
 
     override fun doWork(): Result {
         val prefManager =
@@ -35,7 +56,7 @@ class NotificationWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, p
             applicationContext,
             0,
             Intent(applicationContext, DetailHabitActivity::class.java),
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
         val notificationManager =
